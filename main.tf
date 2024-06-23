cd423a3ee605510851bc1e253194efa40852e8df
Likhitha-user <159176893+Likhitha-user@users.noreply.github.com>
2024-06-23 12:37:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ pipeline {
                  script{
                         dir("terraform")
                         {
-                            git "https://github.com/yeshwanthlm/Terraform-Jenkins.git"
+                            git "git@github.com:Likhitha-user/Terraform-Jenkins.git"
                         }
                     }
                 }
